9391bceda4b18c075cbed1125fd17e22a9226004
Pavel Kondybailo <pavelkondybailo@yandex.by>
2022-04-04 22:05:24 +0300

    feat: add labels table

---
+++ database/migrations/2022_04_04_182048_create_label_task_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('label_task', function (Blueprint $table) {
+            $table->unsignedBigInteger('task_id');
+            $table->unsignedBigInteger('label_id');
+
+            $table->primary(['task_id', 'label_id']);
+
+            $table->foreign('task_id')->references('id')->on('tasks');
+            $table->foreign('label_id')->references('id')->on('labels');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('label_task');
+    }
+};
