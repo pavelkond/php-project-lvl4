0e31176c69fe21a886087dc7d300ec8805a97f0d
Pavel Kondybailo <pavelkondybailo@yandex.by>
2022-04-12 22:36:25 +0300

    test: fix task status controller tests

--- tests/Feature/TaskStatusControllerTest.php
+++ tests/Feature/TaskStatusControllerTest.php
@@ -30,27 +30,36 @@ class TaskStatusControllerTest extends TestCase
     {
         $response = $this->get(route('task_statuses.index'));
         $response->assertOk();
+        $response->assertSee('Статусы');
     }
 
-    public function testCreateWithoutLogin()
+    public function testCreateWithoutAuth()
     {
         $this->get(route('task_statuses.create'))
             ->assertStatus(403);
+    }
 
+    public function testCreateWithAuth()
+    {
         $this->actingAs($this->user)
             ->get(route('task_statuses.create'))
             ->assertSessionHasNoErrors()
-            ->assertOk();
+            ->assertOk()
+            ->assertSeeInOrder(['Создать статус', 'form' ,'Создать']);
     }
 
-    public function testStore()
+    public function testStoreWithoutAuth()
     {
         $data = ['name' => 'status1'];
-        $this->post(route('task_statuses.store'))
+        $this->post(route('task_statuses.store'), $data)
             ->assertStatus(403);
 
         $this->assertDatabaseMissing('task_statuses', $data);
+    }
 
+    public function testStoreWithAuth()
+    {
+        $data = ['name' => 'status1'];
         $this->actingAs($this->user)
             ->post(route('task_statuses.store'), $data)
             ->assertRedirect(route('task_statuses.index'))
@@ -59,39 +68,61 @@ class TaskStatusControllerTest extends TestCase
         $this->assertDatabaseHas('task_statuses', $data);
     }
 
-    public function testEdit()
+    public function testEditWithoutAuth()
     {
         $this->get(route('task_statuses.edit', $this->taskStatus))
             ->assertStatus(403);
+    }
 
+    public function testEditWithAuth()
+    {
         $this->actingAs($this->user)
             ->get(route('task_statuses.edit', $this->taskStatus))
             ->assertOk()
-            ->assertSessionHasNoErrors();
+            ->assertSessionHasNoErrors()
+            ->assertSeeInOrder([
+                'Изменение статуса',
+                'form',
+                $this->taskStatus->getAttribute('name'),
+                'Обновить'
+            ]);
     }
 
-    public function testUpdate()
+    public function testUpdateWithAuth()
     {
         $data = ['name' => 'status1'];
-
-        $this->patch(route('task_statuses.update', $this->taskStatus))
+        $this->patch(route('task_statuses.update', $this->taskStatus), $data)
             ->assertStatus(403);
 
         $this->assertDatabaseMissing('task_statuses', $data);
+    }
 
+    public function testUpdateWithoutAuth()
+    {
+        $data = ['name' => 'status1'];
         $this->actingAs($this->user)
             ->patch(route('task_statuses.update', $this->taskStatus), $data)
             ->assertRedirect(route('task_statuses.index'))
             ->assertSessionHasNoErrors();
 
         $this->assertDatabaseHas('task_statuses', $data);
+        $this->assertDatabaseMissing('task_statuses', [
+            'name' => 'test'
+        ]);
     }
 
-    public function testDestroy()
+    public function testDestroyWithoutAuth()
     {
         $this->delete(route('task_statuses.destroy', $this->taskStatus))
             ->assertStatus(403);
 
+        $this->assertDatabaseHas('task_statuses', [
+            'name' => 'test'
+        ]);
+    }
+
+    public function testDestroyWithAuth()
+    {
         $this->actingAs($this->user)
             ->delete(route('task_statuses.destroy', $this->taskStatus))
             ->assertRedirect(route('task_statuses.index'))
